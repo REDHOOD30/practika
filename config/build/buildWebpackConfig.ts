1519345e7c5aac2ad6b974e224674de1420960c7
artem <artem-seregin-1994@mail.ru>
2025-06-14 23:20:57 +0300

    initial commit

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -1,6 +1,5 @@
 import {BuildOptions} from "./types/config";
 import webpack from "webpack";
-import path from "node:path";
 import {buildLoaders} from "./buildLoaders";
 import {buildResolvers} from "./buildResolvers";
 import {buildPlugins} from "./buildPlugins";
