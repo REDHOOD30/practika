1f10c7f9005629f06c5c109cd1b9034659983f16
artem <artem-seregin-1994@mail.ru>
2025-07-31 11:53:07 +0300

    fix test

--- config/jest/jest.config.ts
+++ config/jest/jest.config.ts
@@ -44,7 +44,7 @@ const config: Config = {
         '<rootDir>src/**/*(*.)@(spec|test).[tj]s?(x)'
     ],
     rootDir: '../../',
-    setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
+    //setupFilesAfterEnv: ['<rootDir>config/jest/setupTests.ts'],
     moduleNameMapper: {
         // '\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
         //     '<rootDir>/__mocks__/fileMock.js',
